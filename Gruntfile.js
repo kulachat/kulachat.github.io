6825c50025c50bedbec45ae3735d3e8c013ee596
kulachat.k <freelancemans@gmail.com>
2018-11-15 23:54:50 +0700

    Update Product page

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/style.css' : 'sass/style.scss',
+          'css/geeko.css' : 'sass/gekko.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'sass/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ['css/*.css', '**/*.html']
+        },
+        options: {
+          watchTask: true,
+          proxy: 'localhost:8888'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.registerTask('default',['browserSync', 'watch']);
+};
